903c7214b1528c8df6e6f6b23f5929d053f2ac6b
SaymonMagrini <saymomagrini@gmail.com>
2025-11-27 17:07:40 -0300

    config.php

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+class Config
+{
+    private $dbHost = "";
+    private $dbName = "";
+    private $dbUser = "";
+    private $dbPass = "";
+
+
+
+    public function conectar()
+    {
+        try {
+            $connection = new PDO(
+                "mysql:host=$this->dbHost;dbname=$this->dbName",
+                "$this->dbUser",
+                "$this->dbPass"
+            );
+
+            return $connection;
+
+
+        } catch (PDOException $e) {
+            echo '<p>' . $e->getMessage() . '</p>';
+        }
+    }
+}
